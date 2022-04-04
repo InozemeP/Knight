8fea2401ee7c1213d36fb8cdc8bdc6adb68d1841
Haro Isida <ebuovchu@gmail.com>
2022-04-04 19:15:54 +0300

    add new animation for knight

--- assets/scripts/scenes/GameScene.js
+++ assets/scripts/scenes/GameScene.js
@@ -20,27 +20,38 @@ class GameScene extends Phaser.Scene {
     };
 
     createKnightAnimation() {
-    const attackConfig = {
-        key: 'attack',
-        frames: this.anims.generateFrameNames('knight', { prefix: 'attack_B/frame', start: 0, end: 12, zeroPad: 4 }),
-        frameRate: 16
-    };
 
-    this.anims.create(attackConfig);
-};
+        this.anims.create({
+            key: 'attack',
+            frames: this.anims.generateFrameNames('knight', { prefix: 'attack_B/frame', start: 0, end: 12, zeroPad: 4 }),
+            frameRate: 16,
+        });
+
+        this.anims.create({
+            key: 'idle',
+            frames: this.anims.generateFrameNames('knight', { prefix: 'idle/frame', start: 0, end: 5, zeroPad: 4 }),
+            frameRate: 8,
+            repeat: -1,
+        });
+
+    };
 
     createKnight() {
-    const lancelot = this.add.sprite(300, 536, 'knight', 'attack_B/frame0000')
-    lancelot.setOrigin(0.5, 1);
-    lancelot.setScale(8);
-};
-
-    createEventOnKnight() {
-        this.input.on('pointerdown', function () {
-            lancelot.play('attack', true);
-        });
+        this.lancelot = this.add.sprite(300, 536, 'knight', 'attack_B/frame0000')
+        this.lancelot.setOrigin(0.5, 1);
+        this.lancelot.scaleX = 1;
+        this.lancelot.play('idle');
     };
 
+    createEventOnKnight() {
+        this.input.on('pointerdown', () => {
+            if (this.lancelot.anims.getName() === 'idle')
+            {
+                this.lancelot.play('attack');
+                this.lancelot.chain('idle');
+            }
+        });
+    };
 
 }
 
